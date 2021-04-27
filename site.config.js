7a668586a43f0a81d3ceab699e41eaeae593f0e5
문진석 <m_jinseok@naver.com>
2021-04-28 00:19:52 +0900

    fix: fix utterancesGitHubRepo

--- site.config.js
+++ site.config.js
@@ -30,7 +30,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: 'koesnij/blog',
+  utterancesGitHubRepo: 'koesnij/notion-blog',
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
